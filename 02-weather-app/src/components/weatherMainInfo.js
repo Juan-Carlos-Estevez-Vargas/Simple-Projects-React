6fde4623a66ac4aeff6ac88bd5730c441ac19482
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-03-15 19:29:54 -0500

    adición responsive design para teléfono en todo app

--- 02-weather-app/src/components/weatherMainInfo.js
+++ 02-weather-app/src/components/weatherMainInfo.js
@@ -21,6 +21,7 @@ export default function WeatherMainInfo({ weather }) {
         </div>
       </div>
       <iframe
+        title="map"
         src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15057.534307180755!2d${weather.location.lon}5!3d${weather.location.lat}5!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2smx!4v1651103744472!5m2!1sen!2smx`}
         width="100%"
         height="350"
